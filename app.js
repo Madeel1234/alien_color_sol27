dd72020aa1865ac37ef2cf1a97443675e6cc44df
Madeel1234 <164627451+Madeel1234@users.noreply.github.com>
2024-04-14 17:19:49 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+"use strict";
+// Task# 27
+// Alien color 3: Turn your if- else chain from exercise into an if - else chain. 
+// If the alien is green, print a message that the player earned 5 points.
+// If the alien is red, print a message that the player earned 10 points.
+// If the alien is yellow,print a message that the player earned 15 points.
+// write three versions of this program, making sure each message is printed for the appropriate color alien.
+Object.defineProperty(exports, "__esModule", { value: true });
+let alien_color = "green";
+if (alien_color == "green") {
+    console.log("the player earned 5 point");
+}
+else if (alien_color == "red") {
+    console.log("the player earned 10 points");
+}
+else {
+    console.log("the player earned 15 points.");
+}
